fda3ca1d17e3c5812fe91443263b9363640339bd
Luís Flávio <luisflavio360@gmail.com>
2024-12-13 17:09:33 -0300

    cors

--- api-crud/index.js
+++ api-crud/index.js
@@ -5,7 +5,7 @@ const { BodyBuilder } = require('./src/bodybuilder/bodybuilder.entity');
 const { Gym } = require('./src/gym/gym.entity');
 const { Style } = require('./src/style/style.entity');
 const corsOptions = {
-    origin: 'https://prova-lp.vercel.app', // Substitua pelo domínio que você quer permitir
+    origin: 'https://prova-lp-gbqm.vercel.app', // Substitua pelo domínio que você quer permitir
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
